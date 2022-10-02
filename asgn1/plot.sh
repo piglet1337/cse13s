212ffbd4057298c34d88014e1c4092a58538c53d
ryan <rnmiller@ucsc.edu>
2022-10-02 16:51:13 -0700

    finished plot.sh

--- asgn1/plot.sh
+++ asgn1/plot.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-make clean && make collatz
+make clean && make collatz #rebuilds collatz
 
 for i in {2..10000};
 do
 	echo -n "$i ";
-	./collatz -n $i | wc -l;
-done > /tmp/length.dat
+	./collatz -n $i | wc -l; #gets length of collatz function
+done > /tmp/length.dat #fills dat file with n in one column and length in another
 
 for i in {2..10000};
 do
 	echo -n "$i ";
-	./collatz -n $i | sort -n | tail -1;
-done > /tmp/max.dat
+	./collatz -n $i | sort -n | tail -1; #gets maximum value of collatz function
+done > /tmp/max.dat #fills dat file file with n in one column and maximum value in another
 
 for i in {2..10000};
 do
-	echo "$i $(($(./collatz -n $i | sort -n | tail -1)/$i))";
-done > /tmp/diff.dat
+	echo "$i $(($(./collatz -n $i | sort -n | tail -1)/$i))"; #gets n followed by the maximum value divided by n
+done > /tmp/diff.dat #fills dat file with n in one column and maximum value divided by n in another
 
+for i in {2..10000};
+do
+	./collatz -n $i | wc -l; #gets length of collatz function
+done | sort -n | uniq -c | awk '{print $2,$1}' > /tmp/histogram.dat #fills dat file with length in one column and frequency in another
+
+# here-document that is sent to gnuplot
 gnuplot <<END
 	set terminal pdf
 	set output "plots.pdf"
@@ -36,4 +42,12 @@ gnuplot <<END
 	set title "Maximum Value Divided by Starting Value"
 	set yrange [*:50]
 	plot "/tmp/diff.dat" with dots title ""
+
+	set title "Collatz Sequence Length Histogram"
+	set xlabel "length"
+	set ylabel "frequency"
+	set yrange [*:*]
+	set style fill solid 1.0
+	set boxwidth 0.8
+	plot "/tmp/histogram.dat" with boxes title ""
 END
