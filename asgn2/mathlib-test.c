3fe45d4dd981f2d24fa287d04e5c095e4862f5cc
ryan <rnmiller@ucsc.edu>
2022-10-09 20:54:42 -0700

    sin and cos made more inclusive

--- asgn2/mathlib-test.c
+++ asgn2/mathlib-test.c
@@ -21,7 +21,7 @@ int main(int argc, char **argv) {
         case 's':
             printf("  x\t\tsin\t\tLibrary\t\tDifference\n");
             printf("  -\t\t---\t\t-------\t\t----------\n");
-            for (double x = 0.0; x<2*M_PI; x+=0.05*M_PI) {
+            for (double x = 0.0; x<=2*M_PI; x+=0.05*M_PI) {
                 double my = my_sin(x);
                 double libr = sin(x);
                 double diff = (my>libr) ? my-libr : libr-my;
@@ -30,9 +30,9 @@ int main(int argc, char **argv) {
             if (not_all) {break;}
         //c: prints header and output of my_cos cos and their difference.
         case 'c':
-            printf(" x\t\tcos\t\tLibrary\t\tDifference\n");
-            printf(" -\t\t---\t\t-------\t\t----------\n");
-            for (double x = 0.0; x<2*M_PI; x+=0.05*M_PI) {
+            printf("  x\t\tcos\t\tLibrary\t\tDifference\n");
+            printf("  -\t\t---\t\t-------\t\t----------\n");
+            for (double x = 0.0; x<=2*M_PI; x+=0.05*M_PI) {
                 double my = my_cos(x);
                 double libr = cos(x);
                 double diff = (my>libr) ? my-libr : libr-my;
@@ -41,8 +41,8 @@ int main(int argc, char **argv) {
             if (not_all) {break;}
         //S: prints header and output of my_arcsin asin and their difference.
         case 'S':
-            printf(" x\t\tarcsin\t\tLibrary\t\tDifference\n");
-            printf(" -\t\t------\t\t-------\t\t----------\n");
+            printf("  x\t\tarcsin\t\tLibrary\t\tDifference\n");
+            printf("  -\t\t------\t\t-------\t\t----------\n");
             for (double x = -1; x<1; x+=0.05) {
                 double my = my_arcsin(x);
                 double libr = asin(x);
@@ -52,8 +52,8 @@ int main(int argc, char **argv) {
             if (not_all) {break;}
         //C: prints header and output of my_arccos acos and their difference.
         case 'C':
-            printf(" x\t\tarccos\t\tLibrary\t\tDifference\n");
-            printf(" -\t\t------\t\t-------\t\t----------\n");
+            printf("  x\t\tarccos\t\tLibrary\t\tDifference\n");
+            printf("  -\t\t------\t\t-------\t\t----------\n");
             for (double x = -1; x<1; x+=0.05) {
                 double my = my_arccos(x);
                 double libr = acos(x);
@@ -63,8 +63,8 @@ int main(int argc, char **argv) {
             if (not_all) {break;}
         //T: prints header and output of my_arctan atan and their difference.
         case 'T':
-            printf(" x\t\tarctan\t\tLibrary\t\tDifference\n");
-            printf(" -\t\t------\t\t-------\t\t----------\n");
+            printf("  x\t\tarctan\t\tLibrary\t\tDifference\n");
+            printf("  -\t\t------\t\t-------\t\t----------\n");
             for (double x = 1; x<10; x+=0.05) {
                 double my = my_arctan(x);
                 double libr = atan(x);
@@ -74,8 +74,8 @@ int main(int argc, char **argv) {
             if (not_all) {break;}
         //l: prints header and output of my_log log and their difference.
         case 'l':
-            printf(" x\t\tlog\t\tLibrary\t\tDifference\n");
-            printf(" -\t\t---\t\t-------\t\t----------\n");
+            printf("  x\t\tlog\t\tLibrary\t\tDifference\n");
+            printf("  -\t\t---\t\t-------\t\t----------\n");
             for (double x = 1; x<10; x+=0.05) {
                 double my = my_log(x);
                 double libr = log(x);
