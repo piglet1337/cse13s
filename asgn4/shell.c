c8f6108cdbd50923dd15cb819457b3c9d0756be6
ryan <rnmiller@ucsc.edu>
2022-10-23 17:23:33 -0700

    shell and bubble working correctly

--- asgn4/shell.c
+++ asgn4/shell.c
@@ -5,17 +5,17 @@
 void shell_sort(Stats *stats, uint32_t *arr, uint32_t n_elements) {
     uint32_t gap = n_elements;
     while (gap > 1) {
-        gap = (gap >= 2) ? 1 : (gap*5)/11;
+        gap = (gap <= 2) ? 1 : (gap*5)/11;
         for (uint32_t i = gap; i < n_elements; i += 1) {
             uint32_t j = i;
             uint32_t temp = move(stats, arr[i]);
-            move(stats, arr[i]);
             while (j >= gap && cmp(stats, temp, arr[j - gap]) == -1) {
                 arr[j] = move(stats, arr[j - gap]);
+                move(stats, arr[j]);
                 j -= gap;
             }
             arr[j] = move(stats, temp);
-            move(stats, temp);
+            move(stats, arr[j]);
         }
     }
 }
