ada3554ea5b76f21280795664a56624b8cd55549
ryan <rnmiller@ucsc.edu>
2022-11-10 02:13:24 -0800

    add given files

---
+++ asgn6/parser.h
@@ -0,0 +1,17 @@
+#ifndef __PARSER_H__
+#define __PARSER_H__
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_PARSER_LINE_LENGTH 1000
+
+typedef struct Parser Parser;
+
+Parser *parser_create(FILE *f);
+
+void parser_delete(Parser **p);
+
+bool next_word(Parser *p, char *word);
+
+#endif
