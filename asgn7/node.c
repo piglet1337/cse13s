97d1283788028d91a6faa79f1db7ae198629582e
ryan <rnmiller@ucsc.edu>
2022-12-02 17:24:20 -0800

    add comments to node.c

--- asgn7/node.c
+++ asgn7/node.c
@@ -1,9 +1,11 @@
+//include necessary libraries
 #include "node.h"
 #include <stdio.h>
 #include <inttypes.h>
 #include <stdlib.h>
 #include <ctype.h>
 
+//Creates node with symbol = symbol and frequency = frequency and returns it
 Node *node_create(uint8_t symbol, uint64_t frequency) {
     Node *n = (Node *) malloc(sizeof(Node));
     n->symbol = symbol;
@@ -11,12 +13,15 @@ Node *node_create(uint8_t symbol, uint64_t frequency) {
     return n;
 }
 
+//frees memory for node and sets pointer to NULL
 void node_delete(Node **n) {
     (void) free(*n);
     *n = NULL;
     n = NULL;
 }
 
+//Creates a new node with symbol set to $ and frequency set to the sum of frequencies of left and right
+//Sets the nodes left = left and right = right and returns it
 Node *node_join(Node *left, Node *right) {
     Node *n = node_create(36, left->frequency + right->frequency);
     n->left = left;
@@ -24,6 +29,7 @@ Node *node_join(Node *left, Node *right) {
     return n;
 }
 
+//prints nodes symbol and frequency
 void node_print(Node *n) {
     if (!iscntrl(n->symbol) && isprint(n->symbol)) {
         printf("%c ", (char) n->symbol);
@@ -34,11 +40,13 @@ void node_print(Node *n) {
     printf("%" PRIu64 "\n", n->frequency);
 }
 
+//returns true if n->frequency is greater than m->frequency and false otherwise
 bool node_cmp(Node *n, Node *m) {
     if (n->frequency > m->frequency) {return true;}
     return false;
 }
 
+//prints node's symbol
 void node_print_sym(Node *n) {
     if (!iscntrl(n->symbol) && isprint(n->symbol)) {
         printf("%c\n", (char) n->symbol);
