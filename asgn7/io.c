bb9b6a51008f16e94dbf7d936add7cf0deb14caf
ryan <rnmiller@ucsc.edu>
2022-12-02 22:19:19 -0800

    io.c done

---
+++ asgn7/io.c
@@ -0,0 +1,76 @@
+//include libraries
+#include "io.h"
+#include "defines.h"
+#include <unistd.h>
+
+//initiliaze variables
+uint64_t bytes_read = 0;
+uint64_t bytes_written = 0;
+static uint8_t read_buffer[BLOCK];
+static uint16_t read_index = 0;
+static int read_num_bytes;
+static uint8_t code_buffer[BLOCK];
+static uint16_t code_index = 0;
+
+//read nbytes from infile into buf and return the number of bytes read
+int read_bytes(int infile, uint8_t *buf, int nbytes) {
+    int total = 0;
+    while (nbytes != total) {
+        int result = read(infile, buf, nbytes-total);
+        total += result;
+        if (result == 0) {break;}
+    }
+    bytes_read += total;
+    return total;
+}
+
+//writes nbytes from buf into outfile
+int write_bytes(int outfile, uint8_t *buf, int nbytes) {
+    int total = 0;
+    while (nbytes != total) {
+        int result = write(outfile, buf, nbytes-total);
+        total += result;
+        if (result == 0) {break;}
+    }
+    bytes_written += total;
+    return total;
+}
+
+//reads next bit from infile and passes it through bit
+bool read_bit(int infile, uint8_t *bit) {
+    if (read_index == 0) {
+        read_num_bytes = read_bytes(infile, read_buffer, BLOCK);
+        if (read_num_bytes == 0) {return false;}
+    }
+    *bit = ((read_buffer[read_index/8]) & (1UL << read_index%8)) >> read_index%8;
+    read_index += 1;
+    if (read_index/8 == read_num_bytes) {
+        read_index = 0;
+    }
+    return true;
+}
+
+void write_code(int outfile, Code *c) {
+    if (code_index == 0) {
+        for (int i = 0; i < BLOCK; i += 1) {
+            code_buffer[i] = 0;
+        }
+    }
+    for (uint32_t i = 0; i < code_size(c); i += 1) {
+        if (code_get_bit(c, i)) {
+            code_buffer[code_index/8] |= 1UL << code_index%8;
+        }
+        code_index += 1;
+        if (code_index == BLOCK*8) {
+            write_bytes(outfile, code_buffer, BLOCK);
+            code_index = 0;
+            for (int j = 0; j < BLOCK; j += 1) {
+                code_buffer[i] = 0;
+            }
+        }
+    }
+}
+
+void flush_codes(int outfile) {
+    write_bytes(outfile, code_buffer, (code_index/8));
+}
--- asgn7/test.c
+++ asgn7/test.c
@@ -1,8 +1,12 @@
+//include libraries
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
 #include "node.h"
 #include "pq.h"
 #include "code.h"
-#include <inttypes.h>
+#include "io.h"
 
 int main () {
     //node tests
@@ -15,20 +19,28 @@ int main () {
 
     //priority queue tests
     PriorityQueue *pq = pq_create(3);
+    if (pq_size(pq) != 0) {printf("pq_size fail");}
     enqueue(pq, right);
+    if (pq_size(pq) != 1) {printf("pq_size fail");}
     enqueue(pq, left);
+    if (pq_size(pq) != 2) {printf("pq_size fail");}
     enqueue(pq, n);
+    if (pq_size(pq) != 3) {printf("pq_size fail");}
     Node *fail = node_create(16, 72);
     if (enqueue(pq, fail)) {printf("enqueue fail");}
     node_delete(&fail);
     Node *l;
     dequeue(pq, &l);
     if (l != left) {printf("dequeue fail");}
+    if (pq_size(pq) != 2) {printf("pq_size fail");}
     dequeue(pq, &l);
     if (l != right) {printf("dequeue fail");}
+    if (pq_size(pq) != 1) {printf("pq_size fail");}
     dequeue(pq, &l);
     if (l != n) {printf("dequeue fail");}
+    if (pq_size(pq) != 0) {printf("pq_size fail");}
     if (dequeue(pq, &l)) {printf("dequeue fail");}
+    if (pq_size(pq) != 0) {printf("pq_size fail");}
     pq_print(pq);
     pq_delete(&pq);
     node_delete(&n);
@@ -37,20 +49,61 @@ int main () {
 
     //code tests
     Code c = code_init();
+    if (code_size(&c) != 0) {printf("code_size fail\n");}
     code_push_bit(&c, 0);
+    if (code_size(&c) != 1) {printf("code_size fail\n");}
     code_push_bit(&c, 0);
+    if (code_size(&c) != 2) {printf("code_size fail\n");}
     code_push_bit(&c, 1);
+    if (code_size(&c) != 3) {printf("code_size fail\n");}
     uint8_t bit;
     code_pop_bit(&c, &bit);
-    if (!bit) {printf("code fail\n");}
+    if (!bit) {printf("code_pop_bit fail\n");}
+    if (code_size(&c) != 2) {printf("code_size fail\n");}
     code_pop_bit(&c, &bit);
-    if (bit) {printf("code fail\n");}
+    if (bit) {printf("code_pop_bit fail\n");}
+    if (code_size(&c) != 1) {printf("code_size fail\n");}
     code_pop_bit(&c, &bit);
-    if (bit) {printf("code fail\n");}
-    if (code_pop_bit(&c, &bit)) {printf("code fail\n");}
+    if (bit) {printf("code_pop_bit fail\n");}
+    if (code_size(&c) != 0) {printf("code_size fail\n");}
+    if (code_pop_bit(&c, &bit)) {printf("code_pop_bit fail\n");}
+    if (code_size(&c) != 0) {printf("code_size fail\n");}
     for (int i = 0; i < ALPHABET; i += 1) {
-        if (!code_push_bit(&c, 1)) {printf("code fail\n");}
+        if (i % 2 == 0) {
+            if (!code_push_bit(&c, 1)) {printf("code_push_bit fail\n");}
+        }
+        else {
+            if (!code_push_bit(&c, 0)) {printf("code_push_bit fail\n");}
+        }
     }
-    if (code_push_bit(&c, 1)) {printf("code fail\n");}
+    if (code_push_bit(&c, 1)) {printf("code_push_bit fail\n");}
+
+    //io tests
+    fclose(fopen("test.txt", "a"));
+    int test_file = open("test.txt", O_WRONLY);
+    for (int i = 0; i < 129; i += 1) {
+        write_code(test_file, &c);
+    }
+    flush_codes(test_file);
+    close(test_file);
+    test_file = open("test.txt", O_RDONLY);
+    for (int i = 0; i < 33024; i += 1) {
+        if (!read_bit(test_file, &bit)) {printf("io fail\n");}
+        if (i % 2 == 0) {
+            if (bit != 1) {
+                printf("io fail\n");
+                return 0;
+            }
+        }
+        else {
+            if (bit != 0) {
+                printf("io fail\n");
+                return 0;
+            }
+        }
+    }
+    close(test_file);
+    remove("test.txt");
+    
     return 0;
 }
